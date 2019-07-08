d5ee714d852b23ad1f4353a081f77a193716560f
greengrass100 <anku10kumar@gmail.com>
2019-07-08 18:02:28 -0500

    checkpoint committed

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController");
 const validation = require('./validation');
 
 router.get("/users/signup", userController.signUp);
-router.post("/users/signUp", validation.validateUsers, userController.create);
+router.post("/users", validation.validateUsers, userController.create);
 router.get("/users/signin", userController.signInForm);
 router.post("/users/signin", validation.validateUsers, userController.signIn);
 router.get("/users/signout", userController.signOut);
