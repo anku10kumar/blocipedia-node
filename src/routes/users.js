9636acbef55bcf0188c7ae4f8c34c4748a2371f9
greengrass100 <anku10kumar@gmail.com>
2019-07-09 12:58:33 -0500

    wip

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const validation = require('./validation');
 
-router.get("/users/signup", userController.signUp);
+router.get("/users/signup", userController.signup);
 router.post("/users", validation.validateUsers, userController.create);
 router.get("/users/signin", userController.signInForm);
 router.post("/users/signin", validation.validateUsers, userController.signIn);
